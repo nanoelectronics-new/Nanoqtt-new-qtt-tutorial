9b1c57d08e0bde99ae618ee9b83c8443dca108bc
nanoelectronics-new <jaime.saezmollejo@ist.ac.at>
2023-04-25 16:46:37 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Nanoqtt'
+copyright = ''
+author = 'Jaime and Yona'
 
 release = '0.1'
 version = '0.1.0'
